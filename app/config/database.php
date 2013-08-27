7079b0af3bfc16ba44d83f31311c12714fae11b2
Jeff Churchill <jeffgit@hodaddy.com>
2013-08-27 11:27:35 -0700

    secure the database credentials

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'ivoter',
-			'username'  => 'ivoterUser',
-			'password'  => 'redbunny5lax',
+			'database'  => 'database',
+			'username'  => 'user',
+			'password'  => 'password',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
